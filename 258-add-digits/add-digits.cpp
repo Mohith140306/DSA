95a92790025b17355cc16b72f99b6ae77bfb7fc6
Dappadi Mohith <mohithdappadi@gmail.com>
2026-09-15 15:19:10 +0530

    Time: 3 ms (3.39%) | Memory: 8.7 MB (45.53%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<=9)
+        {
+            return num;
+        }
+        while(num>=10)
+        {
+            int sum=0;
+            while(num>0)
+            {
+                int d=num%10;
+                sum=sum+d;
+                num=num/10;
+            }
+                num=sum;
+        }
+        return num;
+    }
+};
